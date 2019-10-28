1d720ab52b8cb3a7c139f8f901e7263fda323dca
= <=>
2019-10-28 15:58:10 +0200

    wip

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import {AfterViewInit, Component, OnInit} from "@angular/core";
 import {ListViewScrollEventData, RadListView} from "nativescript-ui-listview";
 import {EventData, Page, View} from "tns-core-modules/ui/page";
+import {screen} from "tns-core-modules/platform/platform"
 import {
     Animation, AnimationDefinition,
     CubicBezierAnimationCurve
@@ -16,49 +17,38 @@ export class HomeComponent implements OnInit, AfterViewInit {
     animation: Animation = undefined;
     dock: View
     bar: View
-    listView: RadListView
+    listView: View
     pageLoaded = false;
-    scrolledEvent;
+    _screenHeight;
     lastScroll;
     stopEvent = false;
     dockedLabelOpacity: number = 0;
-    listViewMargin = -80;
+    listViewMargin = 80;
     items: any[] = [];
 
     constructor(private page: Page) {
     }
 
     onLoaded(args: EventData) {
-        // if ( args.object) {
-        //     layout = args.object as View;
-        //     dock = layout.page.getViewById('dock') as View
-        //     bar = layout.page.getViewById('bar') as View;
-        //     listView = layout.page.getViewById('listView') as View;
-        // }
     }
 
     upAnimation() {
-
         // if (!this.animation) return;
         if (this.animation && this.animation.isPlaying && !this.stopEvent) {
-           //  this.animation.cancel();
-          //  return;
+            //   this.animation.cancel();
+               return;
         }
-        // if (this.listView) {
-        //    this.listView.marginTop = {unit: "dip", value: this.listViewMargin};
-        // } else {
-        //     console.log('No List View...')
-        // }
 
         const def1: AnimationDefinition = {
             target: this.bar,
-            duration: 250,
+            duration: 100,
             translate: {
-                y: 0,
+                y: -80,
                 x: 0
             },
-          //  height: this.listViewMargin * -1 + '',
             opacity: 0,
+            //   height: 0,
+
             curve: new CubicBezierAnimationCurve(0, .76, 0, .94)
         }
         const def2: AnimationDefinition = {
@@ -68,13 +58,18 @@ export class HomeComponent implements OnInit, AfterViewInit {
                 y: 0,
                 x: 0
             },
+            opacity: 1,
             curve: new CubicBezierAnimationCurve(0, .76, 0, .94)
         };
-        this.animation = new Animation([def1, def2]);
+        this.animation = new Animation([def2, def1]);
+        this.listView.marginTop = -40;
         this.animation.play().then(() => {
             this.stopEvent = true;
-            this.dockedLabelOpacity = 1
-        }, (err) => { console.log('canceled...') }).catch(() => {
+            this.dockedLabelOpacity = 1;
+            return;
+        }, (err) => {
+            console.log('canceled...')
+        }).catch(() => {
             console.log('canceleds...')
         });
     }
@@ -83,18 +78,19 @@ export class HomeComponent implements OnInit, AfterViewInit {
         if (!this.animation || !this.pageLoaded) return;
         if (this.animation && this.animation.isPlaying && !this.stopEvent) {
             // console.log(this.animation)
-            // return;
-            this.animation.cancel();
+              return;
+            //this.animation.cancel();
         }
-     //  this.listView.marginTop = {unit: "dip", value: 0};
+          this.listView.marginTop = 0 ;
 
         const def1: AnimationDefinition = {
             target: this.bar,
-            duration: 250,
+            duration: 100,
             translate: {
-                y: -50,
+                y: 0,
                 x: 0
             },
+            //    height: 80,
             opacity: 1,
             curve: new CubicBezierAnimationCurve(0, .76, 0, .94)
         }
@@ -102,27 +98,30 @@ export class HomeComponent implements OnInit, AfterViewInit {
             target: this.dock,
             duration: 150,
             translate: {
-                y: 0,
+                y: -80,
                 x: 0
             },
             opacity: 0,
+          //  height: this.listViewMargin,
             curve: new CubicBezierAnimationCurve(0, .76, 0, .94)
         };
-        this.animation = new Animation([def1, def2])
+        this.animation = new Animation([def2, def1])
 
         this.animation.play().then(() => {
             this.stopEvent = true;
             this.dockedLabelOpacity = 0
-        }, (err) => { console.log('canceled...') }).catch(() => {
+            return;
+        }, (err) => {
+            console.log('canceled...')
+        }).catch(() => {
             console.log('canceled...')
         });
     }
 
     onScroll(args: ListViewScrollEventData) {
-        // if (!this.listView) {
-        //     this.listView = this.page.getViewById<View>('listView')
-        // }
-
+        if (!this.lastScroll) {
+            this.lastScroll = args.scrollOffset;
+        }
         if (args && args.scrollOffset && args.scrollOffset > 1) {
             if (this.lastScroll < args.scrollOffset) {
                 this.upAnimation();
@@ -140,26 +139,28 @@ export class HomeComponent implements OnInit, AfterViewInit {
     onScrollEnded(args: ListViewScrollEventData) {
         if (args && args.scrollOffset && args.scrollOffset >= 0) {
             this.lastScroll = args.scrollOffset;
-
-         //   this.animation = undefined;
         }
     }
 
     ngOnInit(): void {
+        this.screenHeight = 0;
         this.items = this.createItems();
     }
 
     ngAfterViewInit(): void {
         this.dock = this.page.getViewById<View>('dock')
         this.bar = this.page.getViewById<View>('bar')
-        this.listView = this.page.getViewById<RadListView>('listView')
-
+        this.listView = this.page.getViewById<View>('listView')
+        this.listView.marginTop = 0;
         this.pageLoaded = true;
-        console.log('*****')
-        console.log(this.dock)
-        console.log(this.bar)
-        console.log(this.listView)
-        console.log('**2***')
+    }
+
+    get screenHeight() {
+        return this._screenHeight;
+    }
+
+    set screenHeight(height: number) {
+        this._screenHeight = screen.mainScreen.heightPixels - height;
     }
 
     createItems() {
@@ -167,7 +168,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
         for (let i = 0; i <= 100; i++) {
             items.push({
                 name: 'name ' + i,
-                id: 'id'+i,
+                id: 'id' + i,
             })
         }
         return items;
